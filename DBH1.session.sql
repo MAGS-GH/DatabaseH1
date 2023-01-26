71ec41dd21c55f966e8a230fb9d234dadc20d2c4
MathiasGS <mathiasgaardsdalsteenberg@gmail.com>
2023-01-26 09:17:37 +0100

    first commit

---
+++ DBH1.session.sql
@@ -0,0 +1,66 @@
+-- @block
+SHOW DATABASES;
+
+-- @block delete the student table
+DROP TABLE IF EXISTS student;
+-- @block
+CREATE TABLE Student (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    height FLOAT, 
+    averageGrade FLOAT
+);
+
+-- @block 
+INSERT INTO Student (email, height, averageGrade)
+VALUES (
+    'Anders@edu.com',
+    177.5,
+    5.4
+);
+
+-- @block 
+INSERT INTO Student (email, height, averageGrade)
+VALUES ('Karsten@edu.com', 180.3, 9.6),
+       ('Trine@edu.com', 167.5, 11),
+       ('Signe@edu.com', 163, 3.4);
+
+
+-- @block
+SELECT * FROM student;
+
+-- @block
+SELECT email, id FROM student
+LIMIT 2;
+
+-- @block
+SELECT email, id FROM student
+ORDER BY id DESC
+LIMIT 2;
+
+-- @block
+SELECT email, id FROM student
+
+WHERE height > 170
+
+ORDER BY id DESC
+LIMIT 2;
+
+-- @block
+SELECT email, id FROM student
+
+WHERE height > 170 and averageGrade > 7
+
+ORDER BY id DESC
+LIMIT 2;
+
+-- @block
+SELECT email, id FROM student
+
+WHERE height > 170 or averageGrade > 7
+
+ORDER BY id DESC
+LIMIT 2;
+
+-- @block
+CREATE INDEX email_index ON student(email)
