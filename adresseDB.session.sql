73390c66101c3fdeaa0c5d3febfd56507a9632ae
MAGS <mathiasgaardsdalsteenberg@gmail.com>
2023-03-28 10:58:26 +0200

    Init

--- adresseDB.session.sql
+++ adresseDB.session.sql
@@ -1,21 +1,3 @@
--- @block 
-SELECT vejnavn, postnrnavn FROM adresse
-WHERE vejnavn = "Ahornvej"
-
--- @block
+-- @block her kan i teste jeres database:
 SELECT * FROM adresse
 WHERE id = "eaaf1ca0-ec82-4ae5-9b52-1748404ad8c3"
-
--- @block
-SELECT * FROM adresse
-WHERE vejnavn = "Aaboulevarden"
-
--- @block
-
--- @block
-
--- @block
-
--- @block
-
-
